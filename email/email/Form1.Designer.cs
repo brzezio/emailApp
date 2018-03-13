5157ceb4e6501415265ce98b8c6912cab5d00b31
brzezio <damian.brzezio97@gmail.com>
2018-03-13 16:39:33 +0100

    Start

---
+++ email/email/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace email
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.labelTo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(264, 244);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(118, 35);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxTo
+            // 
+            this.textBoxTo.Location = new System.Drawing.Point(47, 56);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.Size = new System.Drawing.Size(182, 20);
+            this.textBoxTo.TabIndex = 1;
+            // 
+            // labelTo
+            // 
+            this.labelTo.AutoSize = true;
+            this.labelTo.Location = new System.Drawing.Point(18, 59);
+            this.labelTo.Name = "labelTo";
+            this.labelTo.Size = new System.Drawing.Size(23, 13);
+            this.labelTo.TabIndex = 2;
+            this.labelTo.Text = "To:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 334);
+            this.Controls.Add(this.labelTo);
+            this.Controls.Add(this.textBoxTo);
+            this.Controls.Add(this.buttonSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxTo;
+        private System.Windows.Forms.Label labelTo;
+    }
+}
+
---
+++ email/email/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace email
+{
+    public partial class Form1 : Form
+    {
+        public string userEmail;
+        public string userLogin;
+        public string userPassword;
+        public string smtpServer;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SmtpClient smtp = new SmtpClient(smtpServer);
+            smtp.Port =587;
+            smtp.Credentials = new System.Net.NetworkCredential(userLogin, userPassword);
+            smtp.EnableSsl = true;
+
+            MailMessage mail = new MailMessage(/*from*/userEmail,
+                                               /*to*/textBoxTo.Text,
+                                               /*subject*/"test stmp",
+                                               /*body*/"stmp test");
+            smtp.Send(mail);
+            MessageBox.Show("Done");
+        }
+
+    }
+}
