f5d38b702831392341a0cba70b3ff78aadd9ca27
brzezio <damian.brzezio97@gmail.com>
2018-03-14 16:17:49 +0100

    TODO: Load on start

--- email/email/Form1.Designer.cs
+++ email/email/Form1.Designer.cs
@@ -34,13 +34,17 @@
             this.textBoxSubject = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.progressBarSend = new System.Windows.Forms.ProgressBar();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // buttonSend
             // 
-            this.buttonSend.Location = new System.Drawing.Point(264, 244);
+            this.buttonSend.Location = new System.Drawing.Point(150, 260);
             this.buttonSend.Name = "buttonSend";
-            this.buttonSend.Size = new System.Drawing.Size(118, 35);
+            this.buttonSend.Size = new System.Drawing.Size(100, 35);
             this.buttonSend.TabIndex = 0;
             this.buttonSend.Text = "Send";
             this.buttonSend.UseVisualStyleBackColor = true;
@@ -48,7 +52,7 @@
             // 
             // textBoxTo
             // 
-            this.textBoxTo.Location = new System.Drawing.Point(58, 34);
+            this.textBoxTo.Location = new System.Drawing.Point(86, 32);
             this.textBoxTo.Name = "textBoxTo";
             this.textBoxTo.Size = new System.Drawing.Size(182, 20);
             this.textBoxTo.TabIndex = 1;
@@ -56,7 +60,7 @@
             // labelTo
             // 
             this.labelTo.AutoSize = true;
-            this.labelTo.Location = new System.Drawing.Point(29, 37);
+            this.labelTo.Location = new System.Drawing.Point(57, 35);
             this.labelTo.Name = "labelTo";
             this.labelTo.Size = new System.Drawing.Size(23, 13);
             this.labelTo.TabIndex = 2;
@@ -64,7 +68,7 @@
             // 
             // textBoxSubject
             // 
-            this.textBoxSubject.Location = new System.Drawing.Point(58, 60);
+            this.textBoxSubject.Location = new System.Drawing.Point(86, 58);
             this.textBoxSubject.Name = "textBoxSubject";
             this.textBoxSubject.Size = new System.Drawing.Size(555, 20);
             this.textBoxSubject.TabIndex = 3;
@@ -72,7 +76,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 63);
+            this.label1.Location = new System.Drawing.Point(34, 61);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 4;
@@ -80,16 +84,57 @@
             // 
             // progressBarSend
             // 
-            this.progressBarSend.Location = new System.Drawing.Point(264, 286);
+            this.progressBarSend.Location = new System.Drawing.Point(86, 303);
             this.progressBarSend.Name = "progressBarSend";
-            this.progressBarSend.Size = new System.Drawing.Size(118, 19);
+            this.progressBarSend.Size = new System.Drawing.Size(555, 19);
             this.progressBarSend.TabIndex = 5;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(86, 85);
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(555, 169);
+            this.textBoxMessage.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Message:";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(431, 260);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(100, 35);
+            this.buttonSave.TabIndex = 8;
+            this.buttonSave.Text = "SaveMessage";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(541, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 35);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "LoadMessage";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(630, 334);
+            this.ClientSize = new System.Drawing.Size(702, 334);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxMessage);
             this.Controls.Add(this.progressBarSend);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxSubject);
@@ -98,6 +143,7 @@
             this.Controls.Add(this.buttonSend);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,6 +157,10 @@
         private System.Windows.Forms.TextBox textBoxSubject;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ProgressBar progressBarSend;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- email/email/Form1.cs
+++ email/email/Form1.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace email
 {
@@ -20,28 +21,30 @@ namespace email
         {
             try
             {
-                userID = System.IO.File.ReadAllLines(@"userID.txt");
+                Load("lastKnownEmailAdress");
+            }
+            catch(Exception)
+            {
+
+            }
+            try
+            {
+                userID = System.IO.File.ReadAllLines(@"userID");
             }
             catch (Exception e)
             {
                 MessageBox.Show("Błąd odczytu pliku.\n" + e.ToString(),"Error");
-                Environment.Exit(1);
             }
             try
             {
-                if (!(userID[0] == "" || userID[1] == "" || userID[2] == "" || userID[3] == ""))
+                if (userID[0].Length == 0 || userID[1].Length == 0 || userID[2].Length == 0 || userID[3].Length == 0)
                 {
-                    StringBuilder message=new StringBuilder();
-                    message.Append("Witaj");
-                    message.Append(" ");
-                    message.Append(userID[1]);
-                    MessageBox.Show(message.ToString());
+                    throw new System.ArgumentException();
                 }
             }
             catch (Exception)
             {
-                MessageBox.Show("Plik userID.txt nie jest kompletny","Error");
-                Environment.Exit(2);
+                MessageBox.Show("Plik userID.txt nie jest kompletny!","Error");
             }
             InitializeComponent();
         }
@@ -49,27 +52,86 @@ namespace email
         private void button1_Click(object sender, EventArgs e)
         {
             progressBarSend.Value = 0;
-            Thread thread = new Thread(send);
+            Thread thread = new Thread(Send);
             thread.Start();
         }
 
-        private void send()
+        private void Send()
         {
-            SmtpClient smtp = new SmtpClient(userID[3]);
-            progressBarSend.Value = 10;
-            smtp.Port = 587;
-            smtp.Credentials = new System.Net.NetworkCredential(userID[1], userID[2]);
-            smtp.EnableSsl = true;
-            progressBarSend.Value = 30;
+            try
+            {
+                Invoke(new Action(() => { progressBarSend.Value += 20; }));
+                SmtpClient smtp = new SmtpClient(userID[3]);
+                Invoke(new Action(() => { progressBarSend.Value += 20; }));
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential(userID[1], userID[2]);
+                smtp.EnableSsl = true;
+                Invoke(new Action(() => { progressBarSend.Value += 20; }));
 
-            MailMessage mail = new MailMessage(/*from*/userID[0],
-                                               /*to*/textBoxTo.Text,
-                                               /*subject*/textBoxSubject.Text,
-                                               /*body*/"stmp test");
-            progressBarSend.Value = 70;
-            smtp.Send(mail);
-            progressBarSend.Value = 100;
-            MessageBox.Show("Done");
+                MailMessage mail = new MailMessage(/*from*/userID[0],
+                                                   /*to*/textBoxTo.Text,
+                                                   /*subject*/textBoxSubject.Text,
+                                                   /*body*/textBoxMessage.Text);
+                Invoke(new Action(() => { progressBarSend.Value += 20; }));
+                smtp.Send(mail);
+                Invoke(new Action(() => { progressBarSend.Value += 20; }));
+                MessageBox.Show("Done");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Nie powiodło się wysłanie wiadomości! \nSprawdzi dane w pliku userID.txt oraz poprawność adresu email osoby do której piszesz. \nPoprawna kolejność w pliku:\nemail użytkownika\nlogin\nhasło\nsmtp server","Error");
+            }
+            Invoke(new Action(() => { progressBarSend.Value = 0; }));
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Save(saveFileDialog.FileName);
+            }
+        }
+
+        private void Save(string saveFileDialog)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(textBoxTo.Text);
+            stringBuilder.Append("*");
+            stringBuilder.Append(textBoxSubject.Text);
+            stringBuilder.Append("*");
+            stringBuilder.Append(textBoxMessage.Text);
+            File.WriteAllText(saveFileDialog, stringBuilder.ToString());
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Load(openFileDialog.FileName);
+            }
+        }
+
+        private void Load(string openFileDialog)
+        {
+            string read = File.ReadAllText(openFileDialog);
+            string[] splitedTxt = read.Split('*');
+            try
+            {
+                textBoxTo.Text = splitedTxt[0];
+                textBoxSubject.Text = splitedTxt[1];
+                textBoxMessage.Text = splitedTxt[2];
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllText(@"lastKnownEmailAdress",textBoxTo.Text);
         }
     }
 }
